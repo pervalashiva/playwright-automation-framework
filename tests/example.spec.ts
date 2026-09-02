65323b5fe842fa2338add60ae2e084a36524ed55
Shiva <shiva@Shivas-MacBook-Pro.local>
2026-09-02 21:08:34 +0530

    Commit the google search

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,23 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-console.log(page.title());
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test('Navigate the google', async ({ page }) => {
+ 
+    await page.goto('https://www.google.com/');
+await page.waitForTimeout(5000);
+   const searchBox = page.getByRole('combobox', { name: 'Search' });
+await page.waitForTimeout(5000);
+await searchBox.fill('Enter Playwright automation');
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+await expect(searchBox).toHaveValue('Enter Playwright automation');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+await searchBox.press('Enter');
+await page.waitForTimeout(5000);
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+
+
+
+    
+
+
+
 });
