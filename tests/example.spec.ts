99f9ffdbf6828f5cf8379a231e59e019dca24cbe
Shiva <shiva@Shivas-MacBook-Pro.local>
2026-09-02 21:57:37 +0530

    add getBytext

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,21 +2,10 @@ import { test, expect } from '@playwright/test';
 
 test('Navigate the google', async ({ page }) => {
  
-    await page.goto('https://www.google.com/');
-await page.waitForTimeout(5000);
-   const searchBox = page.getByRole('combobox', { name: 'Search' });
-await page.waitForTimeout(5000);
-await searchBox.fill('Enter Playwright automation');
+   await page.goto("https://playwright.dev/")
+  const text = page.getByText('Playwright').first();
+   await expect(text).toBeVisible()
 
-await expect(searchBox).toHaveValue('Enter Playwright automation');
-
-await searchBox.press('Enter');
-await page.waitForTimeout(5000);
-
-
-
-
-    
 
 
 
